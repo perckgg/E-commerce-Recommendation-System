02e4fd11500a55db78b7fc31ed7a18b0d10594c2
perckgg <khangtuyhoa123@gmail.com>
2024-11-11 08:12:30 +0700

    add cmt model

--- app/db_model.py
+++ app/db_model.py
@@ -1,6 +1,6 @@
 from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
-
+import datetime
 db = SQLAlchemy()
 class User(UserMixin,db.Model):
     __tablename__ = "users"
@@ -63,4 +63,12 @@ class Ordered_item(db.Model):
   
 class Commnent(db.Model):
     __tablename__ = "comment"
-    id = db.Col
+    id = db.Column(db.Interger,primary_key = True)
+    content = db.Column(db.Text, nullable = False)
+    create_at = db.Column(db.Datetime,default = datetime.datetime.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('comments', lazy=True))
+    product = db.relationship('Product', backref=db.backref('comments', lazy=True))
+    def __repr__(self):
+        return f"<Comment {self.content[:20]} by User {self.user_id}>"
