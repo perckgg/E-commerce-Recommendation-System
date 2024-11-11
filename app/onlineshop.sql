4aabb6008889b5760c0292b078ddef73ba5350b9
perckgg <khangtuyhoa123@gmail.com>
2024-11-11 08:39:49 +0700

    update admin

--- app/onlineshop.sql
+++ app/onlineshop.sql
@@ -18,6 +18,14 @@ CREATE TABLE items (
 	price_id VARCHAR(250) NOT NULL, 
 	PRIMARY KEY (id)
 );
+CREATE TABLE comment(
+	id INTEGER NOT NULL,
+	content text NOT NULL,
+	product_id INTEGER NOT NULL,
+	user_id INTEGER NOT NULL,
+	FOREIGN KEY user_id REFERENCES users(id),
+	FOREIGN KEY product_id REFERENCES items(id)
+);
 INSERT INTO "items" VALUES(1,'iPhone 12',799.0,'Apple','https://www.gizmochina.com/wp-content/uploads/2020/05/iphone-12-pro-max-family-hero-all-600x600.jpg','6.1-inch OLED display<br>A14 Bionic chip<br>256GB storage','price_1Jk8KjBZlBPWG6ECQXNqcKhR');
 INSERT INTO "items" VALUES(2,'iPhone 12 mini',729.0,'Apple','https://fdn2.gsmarena.com/vv/pics/apple/apple-iphone-12-mini-2.jpg','5.4-inch Super Retina XDR display<br>Dual 12MP camera system<br>256 GB storage','price_1Jk8LrBZlBPWG6ECvsEjYsZF');
 INSERT INTO "items" VALUES(3,'iPhone 11',699.0,'Apple','https://www.gizmochina.com/wp-content/uploads/2019/09/Apple-iPhone-11-1.jpg','A13 Bionic chip<br>smart HDR<br>128GB storage','price_1Jk8MUBZlBPWG6ECueOfWc9N');
